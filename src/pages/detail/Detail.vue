d50fa982490380e6bfd4a7252226b6b8334a3fbd
whtpp <939676512@qq.com>
2022-03-03 20:48:01 +0800

    递归组件已完成

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,24 +2,55 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+
+    <div class="content"><detail-list :list="list"></detail-list></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from "./components/Banner.vue";
 import DetailHeader from "./components/Header.vue";
+import DetailList from "./components/List.vue";
 export default {
   name: "Detail",
-  components: { 
-      DetailBanner,
-      DetailHeader,
-    },
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList,
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [
+            {
+              title: "成人三馆联票",
+              children: [
+                {
+                  title: "成人三馆联票",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: "学生票",
+        },
+        {
+          title: "儿童票",
+        },
+        {
+          title: "特惠票",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
 .content {
-  height:100rem;
+  height: 100rem;
 }
 </style>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="(item, index) of list" :key="index">
+      <div class="item-title">
+        {{ item.title }}
+      </div>
+      <div v-if="item.children">
+        <detail-list :list="item.children" class="item-children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailList",
+  props: {
+    list: Array,
+  },
+};
+</script>
+
+<style scoped>
+.item-title {
+  line-height: 2rem;
+  font-weight: 500;
+  line-height: 3rem;
+
+  border-bottom: 0.05rem solid rgb(116, 155, 175);
+  font-size: 1.2rem;
+  padding: 0 1rem;
+}
+.item-children {
+    padding-left:2rem;
+}
+</style>
